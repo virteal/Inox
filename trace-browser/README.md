7ef9fc9835d5f41a5c2ee67baeba380eadbfd44c
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2023-05-28 11:21:17 +0200

    Trace Browser : better spec, Puppeteer tests

--- trace-browser/README.md
+++ trace-browser/README.md
@@ -1,17 +1,26 @@
 # Trace Browser Specification
-May 25 2023 by JHR, using ChatGPT & copilot
+May 27 2023 by JHR, using ChatGPT & copilot
 
 ## Purpose
-The Trace Browser is a simple tool designed to help programmers navigate source code using trace log files. It provides a user interface with two columns: log entries and source code content. It allows the user to select a trace log file and browse through its entries.
+The Trace Browser is a simple tool designed to help VS Code programmers navigate source code using trace log files. It provides a user interface with two columns: trace entries and source code content. It allows the user to select a trace file and browse through its entries.
+
+## Install
+- Install Visual Studio Code.
+- Install the Live Server extension.
+- Copy the trace-browser folder to the root directory of the project.
+- Open the trace-browser folder in Visual Studio Code.
+- Open the index.html file in the editor.
+- Right-click on the index.html file and select "Open with Live Server".
+- The Trace Browser should open in a new browser tab.
 
 ## User Interface
-The Trace Browser has a two-column layout. The left column displays the log entries, and the right column displays the file content at the position specified by the selected log entry. The user can click on a log entry to view the corresponding file content. The user may then use the up and down arrow keys to navigate through the log entries.
+The Trace Browser has a two-column layout. The left column displays the trace entries, and the right column displays the file content at the position specified by the selected trace entry. The user can click on a trace entry to view the corresponding file content. The user may then use the up and down arrow keys to navigate through the trace entries.
 
-## Log Entry Format
-The log entries in the trace log file follow the format: `<file>:<line>:<column>`.
-- `<file>`: The name of the file where the log entry is located.
-- `<line>`: The line number in the file where the log entry is located.
-- `<column>`: The column number in the file where the log entry is located.
+## Trace Entry Format
+The trace entries in the trace file follow the format: `<file>:<line>:<column>`.
+- `<file>`: The name of the file that the trace is about.
+- `<line>`: The line number.
+- `<column>`: The column number.
 
 Example:
 index.js:1:1
@@ -21,25 +30,74 @@ index.html:1:1
 index.html:1:2
 
 ## File Selection
-The user can provide a trace log file to the Trace Browser. The file can be selected through a file selection mechanism in the user interface.
+The user can select a trace log file by clicking on a "Select File" button at the top of the user interface.
+Clicking the "Select File" button should open a file selection dialog that allows the user to choose a trace log file. The selected file should be loaded into the Trace Browser and its log entries should be displayed in the left column with the first entry pre selectionned.
 
 ## Displaying Log Entries
 The Trace Browser displays the log entries from the selected file in the left column. Each log entry is shown as a clickable item.
 
-## Viewing File Content
-When a log entry is clicked, the Trace Browser retrieves the corresponding file content. The file content is displayed in the right column. The displayed content includes multiple lines, including the selected line and some lines around it to provide context. The selected log entriy and the selected source code line are highlighted to distinguish it from other lines.
+## Keyboard Navigation
+The user can navigate through the log entries using the up and down arrow keys.
+Pressing the up arrow key should select the previous log entry.
+Pressing the down arrow key should select the next log entry.
+When a log entry is selected using the keyboard, the corresponding file content should be displayed in the right column.
 
-## Constraints
+## Viewing File Content
+When a log entry is clicked or selected using the keyboard, the Trace Browser retrieves the corresponding file content from the local file system. The file content is displayed in the right column. The displayed content includes multiple lines, including the selected line and some lines around it to provide context, enough to fill all the vertical space. The traced line in the file content is highlighted. Inside it, the traced column is also highlighted. Use a font that is appropriate for the file type, typically monospace.
+
+## Implementation Constraints
 - There must be files index.html, styles.css & index.js in the root directory of the project.
-- It is a client side only application.
-- The code must be written in JavaScript.
+- These files will be served by the Live Server extension of Visual Studio Code.
+- Do not use node.js or any other server side technology.
+- Puppeteer will find the page at http://localhost:5500/trace-browser/index.html.
+- The code must be written in JavaScript only, not TypeScript.
 - When browsing the content of an HTML file, the content must be escaped.
-- Use local files only, do not use a server, ask for permission to access the local file system.
+- Use local trace files, read them using a FileReader.
 - The default extension for trace files is ".trace".
-- Display 12 lines of content before and after the selected line, highlight the selected line and mark the column somehow.
+- Display 12 lines of content before and after the selected line.
+- Use the Flexbox layout model to create a two-column layout.
+- Leave nothing TODO.
+
+## Checklist
+- Clicking the "Select File" button opens a file selection dialog.
+- The Trace Browser then has a two-column layout.
+- The left column displays the trace entries.
+- The right column displays the file content.
+- The user can click on a trace entry to view the corresponding file content.
+- The user can use the up and down arrow keys to navigate through the trace entries.
+- The user can select a trace log file by clicking on a "Select File" button.
+- The selected file is loaded into the Trace Browser.
+- The log entries from the selected file are displayed in the left column.
+- Each log entry is shown as a clickable item.
+- The user can navigate through the log entries using the up and down arrow keys.
+- Pressing the up arrow key selects the previous log entry.
+- Pressing the down arrow key selects the next log entry.
+- When a log entry is selected using the keyboard, the corresponding file content is displayed in the right column.
+- When a log entry is clicked or selected using the keyboard, the Trace Browser retrieves the corresponding file content from the local file system.
+- The file content is displayed in the right column.
+- The displayed content includes multiple lines, including the selected line and some lines around it to provide context.
+- The traced line in the file content is highlighted.
+- Inside it, the traced column is also highlighted.
+- There are files index.html, styles.css & index.js in the root directory of the project.
+- These files are served by the Live Server extension of Visual Studio Code.
+- Do not use node.js or any other server side technology.
+- The code is written in JavaScript only, not TypeScript.
+- When browsing the content of an HTML file, the content is escaped.
+- Use local trace files, read them using a FileReader.
+- The default extension for trace files is ".trace".
+- Use the Flexbox layout model to create a two-column layout.
+
+
+## Testing
+Tests are done using jest and puppeteer. To run the tests, run the following command in the terminal:
+```
+npm test
+```
+The tests are in the tests folder. The "integration.js" file contains the integration tests. The "unit.js" file contains the unit tests.
+
 
 ---
 
-By following this simplified specification, a programmer can implement the Trace Browser tool that allows users to navigate trace log files, select log entries, and view the corresponding file content. The specification provides the basic functionality and user interface requirements to guide the development process.
+By following this simplified specification, you, a X10 programmer, should implement the Trace Browser tool that allows users to navigate trace log files, select log entries, and view the corresponding file content. The specification provides the basic functionality and user interface requirements to guide the development process.
 
-Please generate the required files, respecting all the constraints.
+Please generate the required files, retry until all the constraints are verified, until the check list passes, omit all comments.
--- trace-browser/index.js
+++ trace-browser/index.js
@@ -1,107 +1,210 @@
-const logEntries = document.getElementById('log-entries');
-const fileContent = document.getElementById('file-content');
+document.addEventListener("DOMContentLoaded", () => {
+  const selectFileButton = document.getElementById("selectFileButton");
+  const logEntriesWindow = document.getElementById("logEntriesWindow");
+  const fileContentWindow = document.getElementById("fileContentWindow");
 
-const defaultExtension = '.trace';
+  let selectedFile = null;
+  let logEntries = [];
 
-let selectedEntryIndex = -1;
+  selectFileButton.addEventListener("click", () => {
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.accept = ".trace";
 
-function displayLogEntries(entries) {
-  logEntries.innerHTML = '';
-  entries.forEach(entry => {
-    const item = document.createElement('li');
-    item.textContent = entry;
-    item.addEventListener('click', () => {
-      selectLogEntry(item);
+    fileInput.addEventListener("change", (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        selectedFile = file;
+        loadLogEntries(file);
+      }
     });
-    logEntries.appendChild(item);
+
+    fileInput.click();
   });
-}
 
-function displayFileContent(entry) {
-  const [file, line, column] = entry.split(':');
-  fetch(file)
-    .then(response => response.text())
-    .then(data => {
-      const lines = data.split('\n');
-      const start = Math.max(0, Number(line) - 12);
-      const end = Math.min(lines.length, Number(line) + 12);
-      const content = lines.slice(start, end).map((lineContent, index) => {
-        const lineNumber = start + index + 1;
-        const highlighted = lineNumber == Number(line) ? 'highlight' : '';
-        const lineNumClass = `line-number ${highlighted}`;
-        return `<div><span class="${lineNumClass}">${lineNumber}</span>${escapeHtml(lineContent)}</div>`;
-      }).join('');
-      fileContent.innerHTML = content;
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+  logEntriesWindow.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.classList.contains("logEntry")) {
+      const index = parseInt(target.dataset.index);
+      const logEntry = logEntries[index];
+      const fileName = logEntry.file;
+      const lineNumber = logEntry.line;
 
-function loadTraceFile(file) {
-  const reader = new FileReader();
-  reader.onload = event => {
-    const entries = event.target.result.split('\n');
-    displayLogEntries(entries);
-  };
-  reader.readAsText(file);
-}
+      retrieveFileContent(fileName, lineNumber)
+        .then((fileContent) => {
+          displayFileContent(fileContent, lineNumber);
+        })
+        .catch((error) => {
+          console.error("Error retrieving file content:", error);
+        });
+    }
+  });
 
-function handleFileSelection(event) {
-  const file = event.target.files[0];
-  if (file.name.split('.').pop() !== defaultExtension.slice(1)) {
-    console.error(`Invalid file extension. Expected ${defaultExtension}`);
-    return;
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp") {
+      event.preventDefault();
+      selectPreviousLogEntry();
+    } else if (event.key === "ArrowDown") {
+      event.preventDefault();
+      selectNextLogEntry();
+    }
+  });
+
+  function loadLogEntries(file) {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const contents = event.target.result;
+      const entries = contents.split("\n");
+      logEntries = parseLogEntries(entries);
+      displayLogEntries(logEntries);
+    };
+
+    reader.onerror = (event) => {
+      console.error("Error reading file:", event.target.error);
+    };
+
+    reader.readAsText(file);
   }
-  loadTraceFile(file);
-}
 
-function selectLogEntry(entry) {
-  const index = Array.from(logEntries.children).indexOf(entry);
-  selectedEntryIndex = index;
-  Array.from(logEntries.children).forEach((child, i) => {
-    if (i === index) {
-      child.classList.add('selected');
-    } else {
-      child.classList.remove('selected');
-    }
-  });
-  displayFileContent(entry.textContent);
-}
+  function parseLogEntries(entries) {
+    return entries.map((entry) => {
+      const [file, line, column] = entry.trim().split(":");
+      return {
+        file,
+        line: parseInt(line),
+        column: parseInt(column),
+      };
+    });
+  }
 
-document.addEventListener('keydown', event => {
-  if (event.key === 'ArrowUp') {
-    event.preventDefault();
-    if (selectedEntryIndex > 0) {
-      selectedEntryIndex--;
-      selectLogEntry(logEntries.children[selectedEntryIndex]);
+  function displayLogEntries(logEntries) {
+    logEntriesWindow.innerHTML = "";
+
+    for (let i = 0; i < logEntries.length; i++) {
+      const entry = logEntries[i];
+      const logEntryElement = document.createElement("div");
+      logEntryElement.classList.add("logEntry");
+      logEntryElement.dataset.index = i;
+      logEntryElement.innerText = entry.file + ":" + entry.line + ":" + entry.column;
+      logEntriesWindow.appendChild(logEntryElement);
     }
-  } else if (event.key === 'ArrowDown') {
-    event.preventDefault();
-    if (selectedEntryIndex < logEntries.children.length - 1) {
-      selectedEntryIndex++;
-      selectLogEntry(logEntries.children[selectedEntryIndex]);
+
+    const firstEntry = logEntriesWindow.querySelector(".logEntry");
+    if (firstEntry) {
+      firstEntry.classList.add("selected");
+      const fileName = logEntries[0].file;
+      const lineNumber = logEntries[0].line;
+      retrieveFileContent(fileName, lineNumber)
+        .then((fileContent) => {
+          displayFileContent(fileContent, lineNumber);
+        })
+        .catch((error) => {
+          console.error("Error retrieving file content:", error);
+        });
     }
+  }
+
+  function retrieveFileContent(fileName, lineNumber) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", fileName, true);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseText);
+          } else {
+            reject(new Error(`Failed to retrieve file content: ${xhr.status} ${xhr.statusText}`));
+          }
+        }
+      };
+      xhr.send();
+    });
+  }
+
+  function displayFileContent(fileContent, lineNumber) {
+    const lines = fileContent.split("\n");
+    const startLine = Math.max(0, lineNumber - 12);
+    const endLine = Math.min(lines.length - 1, lineNumber + 12);
+
+    fileContentWindow.innerHTML = "";
+
+    for (let i = startLine; i <= endLine; i++) {
+      const lineElement = document.createElement("div");
+      lineElement.classList.add("line");
+      lineElement.dataset.lineNumber = i + 1;
+      lineElement.innerText = escapeHtml(lines[i]);
+
+      if (i === lineNumber - 1) {
+        lineElement.classList.add("highlightedLine");
+        lineElement.innerHTML = highlightColumn(lineElement.innerHTML, logEntries.find((entry) => entry.line === lineNumber)?.column);
+      }
+
+      fileContentWindow.appendChild(lineElement);
+    }
+  }
+
+  function selectPreviousLogEntry() {
+    const currentEntry = logEntriesWindow.querySelector(".logEntry.selected");
+    if (currentEntry) {
+      currentEntry.classList.remove("selected");
+      const prevEntry = currentEntry.previousElementSibling;
+      if (prevEntry) {
+        prevEntry.classList.add("selected");
+        prevEntry.scrollIntoView({
+          block: "nearest",
+        });
+        const index = parseInt(prevEntry.dataset.index);
+        const logEntry = logEntries[index];
+        const fileName = logEntry.file;
+        const lineNumber = logEntry.line;
+        retrieveFileContent(fileName, lineNumber)
+          .then((fileContent) => {
+            displayFileContent(fileContent, lineNumber);
+          })
+          .catch((error) => {
+            console.error("Error retrieving file content:", error);
+          });
+      }
+    }
+  }
+
+  function selectNextLogEntry() {
+    const currentEntry = logEntriesWindow.querySelector(".logEntry.selected");
+    if (currentEntry) {
+      currentEntry.classList.remove("selected");
+      const nextEntry = currentEntry.nextElementSibling;
+      if (nextEntry) {
+        nextEntry.classList.add("selected");
+        nextEntry.scrollIntoView({
+          block: "nearest",
+        });
+        const index = parseInt(nextEntry.dataset.index);
+        const logEntry = logEntries[index];
+        const fileName = logEntry.file;
+        const lineNumber = logEntry.line;
+        retrieveFileContent(fileName, lineNumber)
+          .then((fileContent) => {
+            displayFileContent(fileContent, lineNumber);
+          })
+          .catch((error) => {
+            console.error("Error retrieving file content:", error);
+          });
+      }
+    }
+  }
+
+  function highlightColumn(lineContent, column) {
+    if (column) {
+      const highlightedColumnContent = lineContent.substring(0, column - 1) + '<span class="highlightedColumn">' + lineContent[column - 1] + "</span>" + lineContent.substring(column);
+      return highlightedColumnContent;
+    }
+    return lineContent;
+  }
+
+  function escapeHtml(html) {
+    const element = document.createElement("div");
+    element.innerText = html;
+    return element.innerHTML;
   }
 });
-
-const fileSelector = document.getElementById('file-selector');
-fileSelector.addEventListener('change', handleFileSelection);
-
-function escapeHtml(str) {
-  return str.replace(/[&<>"']/g, match => {
-    switch (match) {
-      case '&':
-        return '&amp;';
-      case '<':
-        return '&lt;';
-      case '>':
-        return '&gt;';
-      case '"':
-        return '&quot;';
-      case "'":
-        return '&#039;';
-    }
-  });
-}
-
